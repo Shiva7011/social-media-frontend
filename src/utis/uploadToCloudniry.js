b0c91322a68df0b4022b4071787db78e0a93c0a7
shiva singh <sshivasingh503832@gmail.com>
2026-04-01 21:15:58 +0530

    Added frontend code

---
+++ src/utis/uploadToCloudniry.js
@@ -0,0 +1,25 @@
+export const uploadToCloudinary = async (pics,fileType) => {
+    
+    if (pics && fileType) {
+
+        console.log("pics",pics,fileType)
+      
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "social_image");
+      data.append("cloud_name", "dqjvhg7eo");
+  
+      const res = await 
+      fetch(`https://api.cloudinary.com/v1_1/dqjvhg7eo/${fileType}/upload`, {
+        method: "post",
+        body: data,
+      })
+        
+        const fileData=await res.json();
+        console.log("url : ", fileData.url);
+        return fileData.url
+  
+    } else {
+      console.log("error");
+    }
+  };
