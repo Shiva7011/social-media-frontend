dc606a01bb7b85cb04988d71cae59e35e8db356d
shiva singh <sshivasingh503832@gmail.com>
2026-04-03 12:46:50 +0530

    frontend ready for deploy

--- src/config/api.js
+++ src/config/api.js
@@ -1,12 +1,19 @@
 import axios from "axios";
-const jwtToken = localStorage.getItem("jwt")
-export const API_BASE_URL = 'http://localhost:5454';
 
+export const API_BASE_URL = "https://social-media-backend-fvqy.onrender.com";
 
 export const api = axios.create({
-  baseURL: API_BASE_URL, 
+  baseURL: API_BASE_URL,
   headers: {
-    'Authorization':`Bearer ${jwtToken}`,
     'Content-Type': 'application/json',
   },
+});
+
+// interceptor (dynamic token)
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("jwt");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
